ba7a36b7b22a2a6d13fdf630589c28999cb85571
Nikolay Suslov <nsuslovi@gmail.com>
2019-08-15 01:50:33 +0300

    use config instead argv

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
-var fs = require('fs'),
-    argv = require('optimist').argv
+// var fs = require('fs'),
+//     argv = require('optimist').argv
+
+var config = require('./readConfig');
 
 var Gun = require('gun')
-        require('gun/sea')
-        require('gun/lib/path')
-        require('gun/lib/not')
-        require('gun/nts')
+require('gun/sea')
+require('gun/lib/path')
+require('gun/lib/not')
+require('gun/nts')
 
-var ssl = ( argv.s  || argv.ssl );
-var pass = ( ( argv.w) ? ( argv.w) : undefined );
-var sslOptions = {
-    key: ( ( argv.k || argv.key ) ? fs.readFileSync( argv.k || argv.key ) : undefined ),
-    cert: ( ( argv.c || argv.cert ) ? fs.readFileSync( argv.c || argv.cert ) : undefined ),
-    ca: ( ( argv.t || argv.ca ) ? fs.readFileSync( argv.t || argv.ca ) : undefined ),
-    passphrase: JSON.stringify(pass)
-};
-var port = ( ( argv.p || argv.port ) ? ( argv.p || argv.port ) : 8080 );
+config.readConfigFile();
+var conf = config.parseConfigOptions();
 
 
-var config = { port: port};
+var config = { port: conf.port };
 //create the server
 
-if(ssl){
-    config.key = sslOptions.key;
-    config.cert = sslOptions.cert;
-    config.server = require('https').createServer(config, Gun.serve(__dirname));
+if (conf.ssl) {
+    config.server = require('https').createServer(conf.sslOptions, Gun.serve(__dirname));
 } else {
     config.server = require('http').createServer(Gun.serve(__dirname));
 }
 
-global.gun = Gun({web: config.server.listen(config.port), axe: false }); //until: 5000, chunk: 10
+global.gun = Gun({ web: config.server.listen(config.port), axe: false }); //until: 5000, chunk: 10
 console.log('Relay peer started on port ' + config.port + ' with /gun');
