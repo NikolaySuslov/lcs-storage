bfcad0a5483b0e85ba1d0ea1654877e91fd1260e
Nikolay Suslov <nsuslovi@gmail.com>
2019-10-05 14:02:11 +0300

    update

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('gun/sea')
 require('gun/lib/path')
 require('gun/lib/not')
 require('gun/nts')
+require('gun/lib/bye')
 
 config.readConfigFile();
 var conf = config.parseConfigOptions();
@@ -22,5 +23,29 @@ if (conf.ssl) {
     config.server = require('http').createServer(Gun.serve(__dirname));
 }
 
-global.gun = Gun({ web: config.server.listen(config.port), axe: false }); //until: 5000, chunk: 10
+global.gun = Gun({ web: config.server.listen(config.port), axe: false}); //until: 5000, chunk: 10 
 console.log('Relay peer started on port ' + config.port + ' with /gun');
+
+let tickMsg = {
+    parameters: [],
+    time: 'tick'//hb
+};
+
+global.gun.get('server').get('heartbeat').get('tick').put(JSON.stringify(tickMsg));
+
+//instance.get('heartbeat').put({ tick: "{}" }).heartbeat(0.0, 1);
+
+setInterval(function () {
+
+    let message = {
+        parameters: [],
+        time: 'tick'//hb
+    };
+
+    global.gun.get('server').get('heartbeat').get('tick').put(JSON.stringify(message),function(ack){
+        if(ack.err){ 
+            console.log('ERROR: ' + ack.err)
+             
+        }});
+
+}, 50); //FIX ERR: syncing c
